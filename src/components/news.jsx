66f19bac94eb2468cb3c67bb259c60e98045c746
rithwik <124823361+rithu453@users.noreply.github.com>
2025-04-19 16:47:58 +0530

    Update news.jsx

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -5,7 +5,7 @@ const News = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/news')
+        fetch('https://lawback-1.onrender.com/news')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
