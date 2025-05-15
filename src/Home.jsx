7ae3bc658b17552b3a621f6489f0e40d1445d039
rithwik <124823361+rithu453@users.noreply.github.com>
2025-05-15 23:59:31 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import LawcardsF from '../components/CardF';
-import LegalNews from '../components/news';
+import LawcardsF from './components/CardF';
+import LegalNews from './components/news';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
